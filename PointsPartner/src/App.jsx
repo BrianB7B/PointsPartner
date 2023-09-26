19b4e18d55a240efffe45da984e71bfa06ae1ca8
Barragan Cesni, Brian Arturo <bbarraga@iu.edu>
2023-09-26 14:09:03 -0400

    working landing Page

--- PointsPartner/src/App.jsx
+++ PointsPartner/src/App.jsx
@@ -2,34 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landingpage from './components/LandingPage';
+import './index.css';
 
-function App() {
+
+export default function App() {
   const [count, setCount] = useState(0)
+  const [appState, setAppState] = useState({});  // assuming an empty object as initial state, adjust as needed
 
   return (
     <>
+      {/* <h1>Vite + Brian</h1> */}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landingpage appState={appState} setAppState={setAppState}/>} />
+          </Routes>
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
---
+++ PointsPartner/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ["./src/**/*.{html,jsx,tsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
